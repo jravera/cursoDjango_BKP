21219acb1c76088bb8022df3b391f1afe526c4ec
jravera <juan.ravera@gmail.com>
2025-01-04 22:13:26 +0100

    Final de formularios_django

--- formularios_django/formularios_django/views.py
+++ formularios_django/formularios_django/views.py
@@ -3,5 +3,13 @@ from django.http      import HttpResponse
 from .forms           import CommentForm
 
 def form(request):
-    comment_form = CommentForm
+    comment_form = CommentForm({'name':'Federico', 'url':'http://google.com','comment':'comentario'})
     return render(request, 'form.html' , {'comment_form':comment_form})
+
+def goal(request):
+    # if request.method != 'GET':   (SOLUCION CON GET)
+    if request.method != 'POST':  # (SOLUCION CON POST)
+        return HttpResponse('Metodo no permitido')
+    
+    #return HttpResponse('nombre:'+ request.GET['name'])   (SOLUCION CON GET)
+    return HttpResponse('nombre:'+ request.POST['name']) # (SOLUCION CON POST)
